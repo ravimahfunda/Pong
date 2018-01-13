f65054a8a2cbae60ff7ff18dc8b430299b2843d7
ravimahfunda <ravimahfunda@gmail.com>
2018-01-13 15:24:39 +0700

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public int force;
+    int scoreP1 = 0;
+    int scoreP2 = 0;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(2,1).normalized * force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector2 direction = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y).normalized;
+            GetComponent<Rigidbody2D>().AddForce(direction * force * 3 / 4);
+        }
+        else if (collision.gameObject.name =="Left") {
+            scoreP2 += 1;
+            ResetBall();
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-2, 2).normalized * force/2);
+        }
+        else if (collision.gameObject.name == "Rigth")
+        {
+            scoreP1 += 1;
+            ResetBall();
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(2, 2).normalized * force/2);
+        }
+
+    }
+
+    void ResetBall()
+    {
+        transform.localPosition = new Vector2(0,0);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
+}
---
+++ Assets/Scripts/PaddleInputHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleInputHandler : MonoBehaviour {
+
+    public int moveSpeed = 3;
+    public string keyControl;
+
+    int currentState;
+    const int NORMAL = 0;
+    const int STUCK_ATAS = 1;
+    const int STUCK_BAWAH = 2;
+
+    // Use this for initialization
+    void Start () {
+        currentState = NORMAL;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float yChange = Input.GetAxis(keyControl) * moveSpeed;
+
+        switch (currentState) {
+            case STUCK_ATAS:
+                if (yChange < 0) {
+                    currentState = NORMAL;
+                }
+                break;
+            case STUCK_BAWAH:
+                if (yChange > 0)
+                {
+                    currentState = NORMAL;
+                }
+                break;
+            default:
+                transform.position = new Vector2(transform.position.x, transform.position.y + yChange);
+                break;
+        }
+        
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Top") {
+            currentState = STUCK_ATAS;
+        }else if (collision.gameObject.name == "Bottom")
+        {
+            currentState = STUCK_BAWAH;
+        }
+    }
+}
